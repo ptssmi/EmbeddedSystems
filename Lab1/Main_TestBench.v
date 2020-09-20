2de737e406afaf25382917e78a21e2612aa67a19
Preston Smith <ptssmith2@gmail.com>
2020-09-20 16:38:15 -0400

    included main test casese

--- Lab1/Main_TestBench.v
+++ Lab1/Main_TestBench.v
@@ -23,20 +23,172 @@
 module Main_TestBench(
 
     );
-    
-
-
-    
     reg [3:0] IN1;
     reg [3:0] IN2;
     wire  [3:0] OUT;
     wire  CF;
     wire  Z;
     reg [1:0] CTRL;
-    reg CLK;
+    reg TOGGLE;
     
+    Main U1(IN1,IN2,OUT,CF,Z,CTRL,TOGGLE);
     
+    initial begin
+    //ADD 
+    CTRL=2'b00;   
+    IN1 = 4'b0000;
+    IN2= 4'b0000;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1111;
+    IN2= 4'b0000;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0000;
+    IN2= 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1100;
+    IN2= 4'b0011;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1001;
+    IN2= 4'b0011;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0100;
+    IN2= 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1111;
+    IN2= 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
     
+    //AND
+    CTRL=2'b01;
+    IN1 = 4'b0000;
+    IN2 = 4'b0000;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1111;
+    IN2= 4'b0000;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0000;
+    IN2= 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1100;
+    IN2= 4'b0011;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1001;
+    IN2= 4'b0011;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0100;
+    IN2= 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1111;
+    IN2= 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
     
+    //NOT
+    CTRL=2'b10;
+    IN1 = 4'b0000;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0001;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0011;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1001;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
     
+    //ZERO
+    CTRL=2'b11;
+    IN1 = 4'b0000;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0001;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0011;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b0111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1001;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    IN1 = 4'b1111;
+    TOGGLE = 1'b1;
+    #10;
+    TOGGLE = 1'b0;
+    #100;
+    
+    end
 endmodule
